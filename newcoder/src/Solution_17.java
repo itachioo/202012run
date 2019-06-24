e45884fcfe9dfcd76bb6276239cbfbf61514d1c2
itachioo <1241040660@qq.com>
2019-06-24 20:49:33 +0800

    顺时针打印矩阵 修改

--- newcoder/src/Solution_17.java
+++ newcoder/src/Solution_17.java
@@ -13,21 +13,18 @@ public class Solution_17 {
         int n = matrix.length-1;
         int m = matrix[0].length-1;
         ArrayList<Integer> res = new ArrayList<>();
-        if(m==0) {
-
-        }
-        for(int i=0;i<=n-i;i++) {
+//        int round = (Math.min(n+1,m+1)+1)>>1;
+        for(int i=0;i<=n-i&&i<=m-i;i++) {
             for(int l=i;l<=m-i;l++) {
                 res.add(matrix[i][l]);
             }
             for(int k=i+1;k<=n-i;k++) {
                 res.add(matrix[k][m-i]);
             }
-            for(int o=m-i-1;o>=i;o--) {
-                if(i==(n+1)/2) continue;
+            for(int o=m-i-1;o>=i&&n-i!=i;o--) {
                 res.add(matrix[n-i][o]);
             }
-            for(int p=n-i-1;p>=i+1;p--) {
+            for(int p=n-i-1;p>=i+1&&i!=m-i;p--) {
                 res.add(matrix[p][i]);
             }
         }
@@ -36,7 +33,7 @@ public class Solution_17 {
 
     @Test
     public void printMatrixTest() {
-        int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int[][] matrix = {{1,2,3,4}};
         ArrayList<Integer> res = printMatrix(matrix);
         for (int i:res) {
             System.out.println(i);
