3a59d0ae8b1b6182f76a8bea987006d7b959f644
itachioo <1241040660@qq.com>
2019-06-26 20:31:13 +0800

    复制复杂链表 不用hashmap实现

--- newcoder/src/Solution_23.java
+++ newcoder/src/Solution_23.java
@@ -29,6 +29,44 @@ public class Solution_23 {
         return map.get(pHead);
     }
 
+    public RandomListNode Clone1(RandomListNode pHead) {
+        /**
+         * 思路：把当前节点的副本放到当前节点和下一个节点之间，作用其实与map一样，
+         * 当前节点 副本节点的next 就是当前节点next节点的next，random就是当前节点
+         * random节点的next节点。
+         * 空间复杂度 ？？？
+         */
+        if(pHead==null) return pHead;
+        RandomListNode cur = pHead;
+        //将当前节点的副本放到cur 和 next之间
+        while(cur!=null) {
+            RandomListNode curNext = cur.next;
+            RandomListNode node = new RandomListNode(cur.label);
+            cur.next = node;
+            node.next = curNext;
+            cur = curNext;
+        }
+        cur = pHead;
+        //修改副本节点的random节点
+        while(cur!=null) {
+            RandomListNode curRandom = cur.random;
+            cur.next.random = curRandom !=null ? curRandom.next : null;
+            cur = cur.next.next;
+        }
+        cur = pHead;
+        RandomListNode next = null;
+        RandomListNode copyCur = null;
+        RandomListNode head = pHead.next;
+        //修改副本节点的next节点
+        while(cur!=null) {
+            next = cur.next.next;
+            copyCur = cur.next;
+            cur.next = next;
+            copyCur.next = next!=null ? next.next : null;
+            cur = cur.next;
+        }
+        return head;
+    }
 
 }
 
