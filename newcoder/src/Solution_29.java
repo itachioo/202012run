228787f218a68d5e37af7de2095e2ef59a52d487
hgang <1241040660@qq.com>
2019-07-11 22:43:48 +0800

    之字形打印二叉树优化

--- newcoder/src/Solution_29.java
+++ newcoder/src/Solution_29.java
@@ -9,29 +9,28 @@ public class Solution_29 {
 	 * 即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
 	 */ 
 	public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
-		Queue<TreeNode> queue1 = new LinkedList<>();
-		Queue<TreeNode> queue2 = new LinkedList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
 		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 		if(pRoot==null) return res;
-		queue1.offer(pRoot);
-		while(!queue1.isEmpty()||!queue2.isEmpty()) {
-			ArrayList<Integer> temp1 = new ArrayList<>();
-			while(!queue1.isEmpty()) {
-				TreeNode node = queue1.poll();
-				temp1.add(node.val);
-				if(node.left!=null) queue2.offer(node.left);
-				if(node.right!=null) queue2.offer(node.right);
+		queue.offer(pRoot);
+		int line = 0;
+		while(!queue.isEmpty()) {
+			ArrayList<Integer> temp = new ArrayList<>();
+			int cnt = queue.size();
+			for(int i=0;i<cnt;i++) {
+				TreeNode node = queue.poll();
+				temp.add(node.val);
+				if(node.left!=null) queue.offer(node.left);
+				if(node.right!=null) queue.offer(node.right);
 			}
-			if(temp1.size()!=0) res.add(new ArrayList<>(temp1));
-			ArrayList<Integer> temp2 = new ArrayList<>();
-			while(!queue2.isEmpty()) {
-				TreeNode node1 = queue2.poll();
-				temp2.add(node1.val);
-				if(node1.left!=null) queue1.offer(node1.left);
-				if(node1.right!=null) queue1.offer(node1.right);
+			if(line%2==0) 
+				res.add(new ArrayList<>(temp));
+			else {
+				Collections.reverse(temp);
+				res.add(new ArrayList<>(temp));
 			}
-			Collections.reverse(temp2);
-			if(temp2.size()!=0)  res.add(new ArrayList<>(temp2));
+			line++;
+			
 		}
 		return res;
 
