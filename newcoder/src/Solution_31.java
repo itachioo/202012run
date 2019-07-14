935840406e75d3740fd68ba10c1341fdf9f61d24
itachioo <1241040660@qq.com>
2019-07-14 21:06:15 +0800

    序列化二叉树第二版

--- newcoder/src/Solution_31.java
+++ newcoder/src/Solution_31.java
@@ -2,25 +2,40 @@ public class Solution_31 {
 	/**
 	 * 题目：请实现两个函数，分别用来序列化和反序列化二叉树
 	 */
-	StringBuilder sb = new StringBuilder();
 	String Serialize(TreeNode root) {
-        if(root==null) return sb.toString();
-        sb.append(root.val+",");
-        Serialize(root.left);
-        if(root.right!=null&& root.left==null) sb.append("#,");
-        Serialize(root.right);
-        if(root.left!=null&& root.right==null) sb.append("#,");
-        return sb.toString();
-  	}
+		StringBuilder sb = new StringBuilder();
+		serHelper(sb, root);
+		sb.deleteCharAt(sb.length()-1);
+		return sb.toString();
+	}
+	void serHelper(StringBuilder sb, TreeNode root) {
+		if(root==null) {
+			sb.append("#,");
+			return;
+		}
+		sb.append(root.val+",");
+		serHelper(sb, root.left);
+		serHelper(sb,root.right);
+
+	}
 
 
-    TreeNode Deserialize(String str) {
+	TreeNode Deserialize(String str) {
+		if(str==null||str.length()==0) return null;
+		String[] arr = str.split(",");
+		int[] idx = new int[1];
+		return deserHelper(arr,idx);
 
-    	TreeNode root = null;
-    	if(str==null||str.length()==0) return root;
-    	for(int i=0;i<str.length();i++) {
-    		root = new TreeNode(Integer.parseInt(str.subString(i,i+1)));
-    	}
-    	
-  	}
+	}
+
+	TreeNode deserHelper(String[] arr,int[] idx) {
+		//if(idx[0]>=arr.length) return null;
+		if(arr[idx[0]].equals("#")) return null;
+		TreeNode root = new TreeNode(Integer.parseInt(arr[idx[0]]));
+		idx[0]++;
+		root.left = deserHelper(arr,idx);
+		idx[0]++;
+		root.right = deserHelper(arr, idx);
+		return root;
+	}
 }
