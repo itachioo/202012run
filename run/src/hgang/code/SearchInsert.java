2419899b1b45055d4b7ed86b11a76d3e722cb43b
itachioo <1241040660@qq.com>
2021-04-07 20:01:05 +0800

    listnode

---
+++ run/src/hgang/code/SearchInsert.java
@@ -0,0 +1,58 @@
+package hgang.code;
+
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+// 你可以假设数组中无重复元素。
+//
+// 示例 1:
+//
+// 输入: [1,3,5,6], 5
+//输出: 2
+//
+//
+// 示例 2:
+//
+// 输入: [1,3,5,6], 2
+//输出: 1
+//
+//
+// 示例 3:
+//
+// 输入: [1,3,5,6], 7
+//输出: 4
+//
+//
+// 示例 4:
+//
+// 输入: [1,3,5,6], 0
+//输出: 0
+//
+// Related Topics 数组 二分查找
+// 👍 782 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+public class SearchInsert{
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 0;
+        searchInsertSolution solution = new searchInsertSolution();
+        System.out.println(solution.searchInsert(nums,target));
+    }
+
+}
+class searchInsertSolution {
+    //查找第一个大于等于target的位置
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length-1;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+            if(target<nums[mid]) r = mid-1;
+            else if(target==nums[mid]) return mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ run/src/hgang/code/SearchRange.java
@@ -0,0 +1,77 @@
+package hgang.code;
+
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+//
+// 进阶：
+//
+//
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4]
+//
+// 示例 2：
+//
+//
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1]
+//
+// 示例 3：
+//
+//
+//输入：nums = [], target = 0
+//输出：[-1,-1]
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums 是一个非递减数组
+// -109 <= target <= 109
+//
+// Related Topics 数组 二分查找
+// 👍 794 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+public class SearchRange{
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 0;
+        searchRangeSolution solution = new searchRangeSolution();
+        solution.searchRange(nums,target);
+    }
+
+}
+
+class searchRangeSolution {
+    public int[] searchRange(int[] nums, int target) {
+        int l = 0, r = nums.length-1;
+        int flag = 0;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+            if (target>nums[mid]) l = mid + 1;
+            else if (target<nums[mid]) r = mid - 1;
+            else {
+                flag = mid;
+                break;
+            }
+        }
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
